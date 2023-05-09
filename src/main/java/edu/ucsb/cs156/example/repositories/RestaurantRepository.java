1c77cf330f30edee1bfe9325e4c1ffac7645d9d4
Ashley Budman <ashleybudman@umail.ucsb.edu>
2023-05-09 15:06:49 -0700

    ab add restaurant entities and repositories

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/RestaurantRepository.java
@@ -0,0 +1,15 @@
+
+
+package edu.ucsb.cs156.example.repositories;
+
+import edu.ucsb.cs156.example.entities.Restaurant;
+
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface RestaurantRepository extends CrudRepository<Restaurant, String> {
+ 
+}
