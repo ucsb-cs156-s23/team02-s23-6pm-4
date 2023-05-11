68676d897a89aeae4e594bc0a503d2e4450573b2
Jessica Ho <shin-yi@umail.ucsb.edu>
2023-05-10 18:10:53 -0700

    add movie table

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/MovieRepository.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs156.example.repositories;
+
+import edu.ucsb.cs156.example.entities.Movie;
+
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MovieRepository extends CrudRepository<Movie, Long> {
+ 
+}
