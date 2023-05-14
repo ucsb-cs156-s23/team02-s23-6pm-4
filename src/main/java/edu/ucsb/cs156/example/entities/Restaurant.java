fe9f6d1fb9ce617a1a0c0f7c4a957735f5eaf537
Ashley Budman <ashleybudman@umail.ucsb.edu>
2023-05-13 20:40:30 -0700

    ab restaurant controller and controllertests

--- src/main/java/edu/ucsb/cs156/example/entities/Restaurant.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Restaurant.java
@@ -1,28 +1,23 @@
 package edu.ucsb.cs156.example.entities;
 
-import java.time.LocalDateTime;
-
 import javax.persistence.Entity;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "restaurants")
+@Entity(name = "restaurant")
 public class Restaurant {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
-
+  private String code;
   private String name;  
-  private String cuisine;
-  private String address;
+  private String descript;
+  private String yelp_rating;
 }
--- src/main/java/edu/ucsb/cs156/example/repositories/RestaurantRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/RestaurantRepository.java
@@ -1,5 +1,3 @@
-
-
 package edu.ucsb.cs156.example.repositories;
 
 import edu.ucsb.cs156.example.entities.Restaurant;
